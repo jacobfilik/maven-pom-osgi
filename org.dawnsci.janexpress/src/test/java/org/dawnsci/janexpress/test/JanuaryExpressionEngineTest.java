ed6b4f5649fa0bce07baa481915ae2a01e69b0a7
Jacob Filik <jacob.filik@diamond.ac.uk>
2020-10-28 22:56:49 +0000

    Add parent pom to try and get CI working

--- org.dawnsci.janexpress/src/test/java/org/dawnsci/janexpress/test/JanuaryExpressionEngineTest.java
+++ org.dawnsci.janexpress/src/test/java/org/dawnsci/janexpress/test/JanuaryExpressionEngineTest.java
@@ -26,22 +26,5 @@ public class JanuaryExpressionEngineTest {
 		
 		assertTrue(Comparisons.equalTo(evaluate, 3).all());
 	}
-	
-	@Test
-	public void runTest2() throws Exception {
-		JanuaryExpressionEngine ee = new JanuaryExpressionEngine();
-		
-		DoubleDataset ones = DatasetFactory.ones(new int[] {4,4});
-		DoubleDataset twos = ones.clone().iadd(1);
-		
-		ee.addLoadedVariable("ones", ones);
-		ee.addLoadedVariable("twos", twos);
-		
-		ee.createExpression("ones+twos");
-		
-		Object evaluate = ee.evaluate();
-		
-		assertTrue(Comparisons.equalTo(evaluate, 4).all());
-	}
 
 }
